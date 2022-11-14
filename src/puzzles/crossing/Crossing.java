43f62241a1e1e84814d163e6628b45826ae3b3be
pgs5983 <solis.patricio02@gmail.com>
2022-11-14 15:07:22 -0500

    CrossingConfig works correctly and returns a solution but still doesn't correctly fucntion for no solution and makes to many configs.

--- src/puzzles/crossing/Crossing.java
+++ src/puzzles/crossing/Crossing.java
@@ -1,13 +1,30 @@
 package puzzles.crossing;
 
+import puzzles.common.solver.Configuration;
 import puzzles.common.solver.Solver;
+
+import java.util.ArrayList;
 
 public class Crossing {
     public static void main(String[] args) {
         if (args.length < 2) {
             System.out.println(("Usage: java Crossing pups wolves"));
         } else {
-            // TODO
+            int pups = Integer.parseInt(args[0]);
+            int wolves = Integer.parseInt(args[1]);
+            System.out.println("Pups: " + pups + ", Wolves: " + wolves);
+
+            CrossingConfig initConfig = new CrossingConfig(pups, wolves);
+
+            ArrayList<Configuration> solution = Solver.solver(initConfig);
+
+            if(solution.isEmpty()){
+                System.out.println("No solution found!");
+            } else {
+                for(int i = 0; i < solution.size(); i++){
+                    System.out.println("Step " + i + ": " + solution.get(i).toString());
+                }
+            }
         }
     }
 }
--- src/puzzles/crossing/CrossingConfig.java
+++ src/puzzles/crossing/CrossingConfig.java
@@ -2,31 +2,88 @@ package puzzles.crossing;
 
 import puzzles.common.solver.Configuration;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class CrossingConfig implements Configuration {
+
+    private int pupsLeft;
+    private int wolvesLeft;
+    private int pupsRight;
+    private int wolvesRight;
+    private boolean boatLeft;
+
+    public CrossingConfig(int pups, int wolves){
+        pupsLeft = pups;
+        wolvesLeft = wolves;
+        pupsRight = 0;
+        wolvesRight = 0;
+        boatLeft = true;
+    }
+
+    public CrossingConfig(CrossingConfig current, int movePups, int moveWolves){
+        if(current.boatLeft){
+            pupsLeft = current.pupsLeft - movePups;
+            pupsRight = current.pupsRight + movePups;
+            wolvesLeft = current.wolvesLeft - moveWolves;
+            wolvesRight = current.wolvesRight + moveWolves;
+            boatLeft = false;
+        } else {
+            pupsLeft = current.pupsLeft + movePups;
+            pupsRight = current.pupsRight - movePups;
+            wolvesLeft = current.wolvesLeft + moveWolves;
+            wolvesRight = current.wolvesRight - moveWolves;
+            boatLeft = true;
+        }
+    }
+
     @Override
     public boolean isSolution() {
-        return false;
+        return pupsLeft == 0 && wolvesLeft == 0;
     }
 
     @Override
     public Collection<Configuration> getNeighbors() {
-        return null;
+        ArrayList<Configuration> successors = new ArrayList<>();
+
+        if(!boatLeft){
+            successors.add(new CrossingConfig(this, 1, 0));
+        } else {
+            successors.add(new CrossingConfig(this, 1, 0));
+            successors.add(new CrossingConfig(this, 2, 0));
+            successors.add(new CrossingConfig(this, 0, 1));
+        }
+
+        return successors;
     }
 
     @Override
     public boolean equals(Object other) {
-        return false;
+        boolean equal = false;
+        if(other instanceof CrossingConfig otherConfig){
+            equal = pupsLeft == otherConfig.pupsLeft && wolvesLeft == otherConfig.wolvesLeft
+                    && boatLeft == otherConfig.boatLeft;
+        }
+        return equal;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        int add = 0;
+        if(boatLeft){
+            add = 1;
+        }
+        return pupsLeft + pupsRight + add;
     }
 
     @Override
     public String toString() {
-        return null;
+        if(boatLeft){
+            return "(BOAT)  left=[" + pupsLeft + ", " + wolvesLeft + "], right=["
+                    + pupsRight + ", " + wolvesRight + "]";
+        } else {
+            return "        left=[" + pupsLeft + ", " + wolvesLeft + "], right=["
+                    + pupsRight + ", " + wolvesRight + "] (BOAT)";
+        }
     }
 }
