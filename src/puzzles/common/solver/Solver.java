f25c5dda9b6de7acd6b5022fd1804de3eedb7c39
pgs5983 <pgs5983@rit.edu>
2022-11-20 13:15:30 -0500

    Solver docstrings added

--- src/puzzles/common/solver/Solver.java
+++ src/puzzles/common/solver/Solver.java
@@ -6,31 +6,42 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * The main Solver class
+ *
+ * @author Patricio Solis
+ */
 public class Solver {
 
+    /**
+     * Solves the puzzle given an initial configuration using BFS
+     *
+     * @param start initial configuration of puzzle
+     * @return ArrayList<Configuration> the list of configurations that lead to the solution in order
+     */
     public static ArrayList<Configuration> solver(Configuration start){
 
-        HashMap<Configuration, Configuration> predecessor = new HashMap<>();
-        predecessor.put(start, null);
+        HashMap<Configuration, Configuration> predecessor = new HashMap<>(); // creates predecessor map
+        predecessor.put(start, null); // puts initial configuration as the start of map
 
-        Queue<Configuration> queue = new LinkedList<>();
-        queue.add(start);
+        Queue<Configuration> queue = new LinkedList<>(); //queue for configurations to be tested
+        queue.add(start); // initial configuration first one to be tested
 
-        int totalConfig = 0;
-        while(!queue.isEmpty() && !queue.peek().isSolution()){
+        int totalConfig = 0; // counts the total number of configurations made
+        while(!queue.isEmpty() && !queue.peek().isSolution()){ // loops until no more configurations or solution is found
             Configuration currConfig = queue.remove();
             for(Configuration neighbor : currConfig.getNeighbors()){
                 totalConfig += 1;
-                if(!predecessor.containsKey(neighbor)){
-                    predecessor.put(neighbor, currConfig);
+                if(!predecessor.containsKey(neighbor)){ // checks if configuration is unique or not
+                    predecessor.put(neighbor, currConfig); // adds configuration to map if unique
                     queue.add(neighbor);
                 }
             }
         }
 
-        if(queue.isEmpty()){
+        if(queue.isEmpty()){ // if no solution is found then it returns an empty ArrayList
             return new ArrayList<>();
-        } else {
+        } else { // if there is a solution it builds the list in reverse order
             ArrayList<Configuration> path = new ArrayList<>();
             path.add(0, queue.peek());
             Configuration config = predecessor.get(queue.peek());
